9cae3682bb95d7a991319236b04ea23ebc2f903b
Chedy Chaaben <chedychaaben@gmail.com>
2025-05-27 01:56:34 +0100

    Update start_browserless.sh

--- scripts/start_browserless.sh
+++ scripts/start_browserless.sh
@@ -7,4 +7,4 @@ export DEFAULT_CHROME_FLAGS="--proxy-server=socks5://xiaomitinyproxy.ddns.net:48
 echo "Launching Browserless with flags: $DEFAULT_CHROME_FLAGS"
 
 # Start Browserless
-exec node ./build/index.js 2>&1
+exec node ./build/index.js 2>&1 --chromeFlags="--proxy-server=socks5://xiaomitinyproxy.ddns.net:48284"
